61448b029ca6cb82dff68adca89d29e6073ad7ff
team5009 <team5009@centrenord.ab.ca>
2018-11-17 12:05:15 -0700

    Surprise ;)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RockyAutonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RockyAutonomous.java
@@ -31,7 +31,7 @@ public class RockyAutonomous extends LinearOpMode {
         if (tfod != null) {
             // getUpdatedRecognitions() will return null if no new information is available since
             // the last time that call was made.
-            List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+           /* List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
             if (updatedRecognitions != null) {
                 telemetry.addData("# Object Detected", updatedRecognitions.size());
                 if (updatedRecognitions.size() == 3) {
@@ -80,18 +80,12 @@ public class RockyAutonomous extends LinearOpMode {
 
                     }
 
-                    robot.pivot(1, 0.6);
-       /*robot.move(new Length( 48,Length.Unit.INCH),-.6);
-        robot.pivot(45, 0.6);
-        robot.marker.setPosition(0.2);
-        TimeUnit.SECONDS.sleep(1);
-       // robot.pivot(135, .6);
-        robot.move(new Length( 70,Length.Unit.INCH), .6);
-        robot.armMove(45,0.6);*/
-
 
                 }
-            }
+            }*/
+                            robot.pivot(15,0.6);
+                             robot.pivot(15,-0.6);
+
         }
     }
 }
