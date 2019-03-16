982ed3b4802ffb06802daadad2b583e0298508d5
team5009 <team5009@centrenord.ab.ca>
2019-03-16 10:44:49 -0600

    GUI, program on the fly

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/k9Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/k9Autonomous.java
@@ -1,14 +1,28 @@
-package org.firstinspires.ftc.teamcode.teleop;
+package org.firstinspires.ftc.teamcode.autonomous;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.hardware.rev.Rev2mDistanceSensor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-//import com.qualcomm.robotcore.hardware.DistanceSensor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.DistanceSensor;
+import org.firstinspires.ftc.teamcode.HardwareRocky;
 
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+
+//import com.qualcomm.robotcore.hardware.DistanceSensor;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.TouchSensor;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
 
 
 /**
@@ -24,51 +38,47 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name = "k9unite", group = "Rocky")
+@TeleOp(name = "k9Autonomous", group = "Rocky")
 
-public class k9unite extends LinearOpMode {
+public class k9Autonomous extends LinearOpMode {
 
-    private DcMotor leftDrive;
-    private DcMotor rightDrive;
-    private DcMotor flagSpinner;
-    private Rev2mDistanceSensor sensorRange;
+    public DcMotor leftDrive;
+    public DcMotor rightDrive;
+    public DcMotor flagSpinner;
+    public Rev2mDistanceSensor sensorRange;
+    public BNO055IMU imu;
 
     @Override
     public void runOpMode() {
         // you can use this as a regular DistanceSensor.
-        leftDrive = hardwareMap.get(DcMotor.class, "left_drive");
-        rightDrive = hardwareMap.get(DcMotor.class, "right_drive");
+        leftDrive = hardwareMap.get(DcMotor.class, "leftDrive");
+        rightDrive = hardwareMap.get(DcMotor.class, "rightDrive");
         flagSpinner = hardwareMap.get(DcMotor.class, "flagSpinner");
         sensorRange = hardwareMap.get(Rev2mDistanceSensor.class,"distanceSensor");
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
 
-        // you can also cast this to a Rev2mDistanceSensor if you want to use added
-        // methods associated with the Rev2mDistanceSensor class.
-
-        //telemetry.addData(">>", "Press start to continue");
         Rev2mDistanceSensor sensorTimeOfFlight = (Rev2mDistanceSensor)sensorRange;
 
 
 
-        telemetry.update();
-
-
-
         waitForStart();
-        while(opModeIsActive()) {
+        while (opModeIsActive()) {
 
-             double leftDrivePower;
-             double rightDrivePower;
-             double flagSpinnerPower;
+            double leftDrivePower;
+            double rightDrivePower;
+            double flagSpinnerPower;
 
 
             leftDrivePower = gamepad1.left_stick_y;
             rightDrivePower = -gamepad1.right_stick_y;
 
-            if (gamepad2.a){flagSpinnerPower = 0.5;}
+            if (gamepad2.y){flagSpinnerPower = 0.5;}
 
-            else if (gamepad2.b){flagSpinnerPower = -0.5;}
+            else if (gamepad2.x){flagSpinnerPower = -0.5;}
 
             else flagSpinnerPower = 0;
+
+
 
             flagSpinner.setPower(flagSpinnerPower);
             leftDrive.setPower(leftDrivePower);
@@ -77,7 +87,14 @@ public class k9unite extends LinearOpMode {
             telemetry.addData("deviceName",sensorRange.getDeviceName() );
             telemetry.addData("Distance", sensorRange.getDistance(DistanceUnit.INCH));
             telemetry.addData("ID", String.format("%x", sensorTimeOfFlight.getModelID()));
+            telemetry.addData("IMU", imu.getAcceleration());
             telemetry.update();
+
+
+
+
+
+
 
             sleep(50);
         }
